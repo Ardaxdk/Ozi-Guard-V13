1b4d19de19e3f19a59827903f61e01017e91a271
Jaylen Ozi <92666466+JaylenOzi@users.noreply.github.com>
2022-04-24 03:55:12 +0300

    Okuyunuz.

---
+++ ozi.js
@@ -0,0 +1,3 @@
+Bu bot guard korumaları tamamen test edilip geliştirilip v13 hale sorunsuz getirilmiştir.
+Almak isteyenler Discord: Jaylen#1404 den fiyat bilgilendirme alabilirler.
+S4tıldıktan sonra bu bot asla paylaşılmayacaktır.
